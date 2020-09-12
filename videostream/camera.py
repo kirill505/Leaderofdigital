7e3e19563d542acc0fc772b46d246ca0e7d582de
Kirill Bazarov <kl.bazarov@gmail.com>
2020-09-12 15:37:30 +0300

    add test video

--- videostream/camera.py
+++ videostream/camera.py
@@ -4,7 +4,8 @@ ds_factor=0.6
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
+        #self.video = cv2.VideoCapture(0)
+        self.video = cv2.VideoCapture('video.mp4')
     
     def __del__(self):
         self.video.release()
