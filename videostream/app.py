d3d9bf30b6dcdaae332656bc99a2d13e437fd96a
kirill <kl.bazarov@gmail.com>
2020-09-14 09:22:53 +0300

    added static files and demo video

--- videostream/app.py
+++ videostream/app.py
@@ -1,67 +1,62 @@
-from flask import Flask, render_template, Response
-import cv2
-
-import os
-import sys
 import cv2
 import time
 import torch
 import utils
 import argparse
-import traceback
 import numpy as np
 
+from flask import Flask, render_template, Response
 from PIL import Image
 from models import gazenet
 from mtcnn import FaceDetector
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='frontend/static',
+            template_folder='frontend/templates')
 
-#camera = cv2.VideoCapture('rtsp://192.168.1.64:8080/h264_pcm.sdp')  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 parser = argparse.ArgumentParser()
 parser.add_argument('--cpu', action='store_true')
-parser.add_argument('--weights','-w', type=str, default='models/weights/gazenet.pth')
+parser.add_argument('--weights', '-w', type=str, default='models/weights/gazenet.pth')
 args = parser.parse_args()
 
 print('Loading MobileFaceGaze model...')
 device = torch.device("cuda:0" if (torch.cuda.is_available() and not args.cpu) else "cpu")
 model = gazenet.GazeNet(device)
 
-if(not torch.cuda.is_available() and not args.cpu):
+if not torch.cuda.is_available() and not args.cpu:
     print('Tried to load GPU but found none. Please check your environment')
 
-    
 state_dict = torch.load(args.weights, map_location=device)
 model.load_state_dict(state_dict)
 print('Model loaded using {} as device'.format(device))
 
 model.eval()
 
-
-#camera = cv2.VideoCapture('rtsp://192.168.1.64:8080/h264_pcm.sdp')
-camera = cv2.VideoCapture('0')
+# camera = cv2.VideoCapture('rtsp://192.168.1.64:8080/h264_pcm.sdp')
+# camera = cv2.VideoCapture('0')
+camera = cv2.VideoCapture('WIN_20200913_12_41_53_Pro.mp4')
 
 face_detector = FaceDetector(device=device)
 
-def gen_frames():  # generate frame by frame from camera
+
+# generate frame by frame from camera
+def gen_frames():
     fps = 0
     frame_num = 0
     frame_samples = 6
     fps_timer = time.time()
     while True:
         # Capture frame-by-frame
-        #success, frame = camera.read()  # read the camera frame
+        # success, frame = camera.read()  # read the camera frame
         success, frame = camera.read()
         if not success:
             break
         else:
-            #ret, buffer = cv2.imencode('.jpg', frame)
-            #frame = buffer.tobytes()
+            # ret, buffer = cv2.imencode('.jpg', frame)
+            # frame = buffer.tobytes()
 
-            
-            
-            frame = frame[:,:,::-1]
+            frame = frame[:, :, ::-1]
             frame = cv2.flip(frame, 1)
             img_h, img_w, _ = np.shape(frame)
             frame_num += 1
@@ -72,32 +67,33 @@ def gen_frames():  # generate frame by frame from camera
             if len(faces) != 0:
                 for f, lm in zip(faces, landmarks):
                     # Confidence check
-                    if(f[-1] > 0.98):
+                    if f[-1] > 0.98:
                         # Crop and normalize face Face
-                        face, gaze_origin, M  = utils.normalize_face(lm, frame)
-                                            
+                        face, gaze_origin, m = utils.normalize_face(lm, frame)
+
                         # Predict gaze
                         with torch.no_grad():
                             gaze = model.get_gaze(face)
-                            gaze = gaze[0].data.cpu()                              
-                        
+                            gaze = gaze[0].data.cpu()
 
-                        # Draw results
-                        display = cv2.circle(display, gaze_origin, 3, (0, 255, 0), -1)            
-                        display = utils.draw_gaze(display, gaze_origin, gaze, color=(255,0,0), thickness=2)
+                            # Draw results
+                        display = cv2.circle(display, gaze_origin, 3, (0, 255, 0), -1)
+                        display = utils.draw_gaze(display, gaze_origin, gaze, color=(255, 0, 0), thickness=2)
 
             # Calc FPS
-            if (frame_num == frame_samples):
+            if frame_num == frame_samples:
                 fps = time.time() - fps_timer
-                fps  = frame_samples / fps;
+                fps = frame_samples / fps
                 fps_timer = time.time()
                 frame_num = 0
-            display = cv2.putText(display, 'FPS: {:.2f}'.format(fps), (0, 20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 1, cv2.LINE_AA)
-            
+            display = cv2.putText(display, 'FPS: {:.2f}'.format(fps), (0, 20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0),
+                                  1, cv2.LINE_AA)
+
             ret, res = cv2.imencode('.jpg', display)
 
+            # concat frame one by one and show result
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + res.tobytes() + b'\r\n')  # concat frame one by one and show result
+                   b'Content-Type: image/jpeg\r\n\r\n' + res.tobytes() + b'\r\n')
 
 
 @app.route('/video_feed')
